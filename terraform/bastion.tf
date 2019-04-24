ce9e0121bca7e71fd59a5a2cf1f0dfe24d393eae
Zhiting Zhu <zhitingz@cs.utexas.edu>
2019-04-24 12:42:41 -0500

    no public key

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -14,7 +14,7 @@ output "bastion_public_ip" {
 
 resource "aws_key_pair" "bastion_key" {
   key_name   = "cluster"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCaPp98f5IPG6A4vL1D1S5cn8ovKAlXr5ckH6vnZ84WlLPyMWahNZHT50daXFpTA4KPBMbaYryE5RN3M41nYY341JtwoTjgAVs49TDMoOmYbWaGKxUSZQvsyOvqC/HL/G02mWj9IMyPMl0bj2EEVmoFLj4814SWmbg2C2heri/7CFVIRP49YNjpOj1sWg04joouG37ngusodF6BizX7UmYip9ZZ6dFtf8R7VxkglXkNhLMxqJleTe9iWTcXC/9vXRy+oyWb1BP4WArsgnhtEQx5KA41QrkasHvcv0AYu9EkQL2xQYW3mSJ5UszowpTdyAuuRfjGmMXUH51TbzquJTHcGEM6FoYqVeES1QN3KrSSEpu7gEAHIek/uYZQ1LQWb6z0L1eUgb2zyitjN3PoxZthwbVtPEfrnYBgjKYiuoSx6og42Us7ku/U4Wusx9N9/6s8Jv2CdLdFXV56fDQzZOnYsIWMm1RsM/NGOJdtm2duQ7j1vo2UCEPIvbsR32RaY7/+yBqbQ/KAjT/WU1PMUSi8Wn7l7oOx+STKpZv5lnRzEIYC7wshaJtBjlYJw9F2bHsXcPtMq64ekuYqm/tblEQCoIcTMxq4updAjDghlxAWDS4JMs8vf2nCruk0oaDJKbRedLtsGk8gX++oboUTQJfB6GuHx1I6u2smVOCJUGjEQ== zhitingz@cash"
+  public_key = ""
 }
 
 resource "aws_security_group" "bastion-sg" {
